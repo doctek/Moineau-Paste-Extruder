7a477e1a679f9377d3e88c75e652409ddd168ae2
Jim Larson <jlarson@pacifier.com>
2014-11-23 13:13:53 -0800

    Added Profiles and Python scripts, GPL info

---
+++ Python/moineau1_triple.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+"""Moineau Pump Profile Generator
+Generate dxfs of Moineau pump stator and rotor profiles
+
+Copyright   2014, James K. Larson
+Version     v0.01 (09/06/14)
+License     GPL
+Homepage    
+
+9/06/14    Initial Creation          -jkl
+This version creates a three lobe stator.
+
+
+Credit to:
+	Thingiverse Moineau Design for inspiration:
+	http://www.thingiverse.com/thing:15538
+	http://www.thingiverse.com/thing:15167
+	(Note there are others that inspired these.)
+	
+	Explanations of Moineau Operation:
+	http://www2.mat.dtu.dk/people/J.Gravesen/MoineauPump/
+	
+	Explanations of cycloid geometry:
+	http://en.wikipedia.org/wiki/Hypocycloid
+	http://en.wikipedia.org/wiki/Epicycloid
+
+    Documenting and updating the sdxf library
+    http://www.kellbot.com/sdxf-python-library-for-dxf/
+
+Notes:
+    Does not currently do ANY checking for sane input values; use at your own risk
+
+"""
+
+import getopt
+import sys
+import sdxf
+import math
+
+
+#
+#	Enter values here to run within the Python IDE.
+# 
+
+
+dxf=sdxf.Drawing()
+
+dxf.layers.append(sdxf.Layer(name="TEXT", color=2) )    #yellow text layer
+dxf.layers.append(sdxf.Layer(name="ROTOR", color=1) )     #red rotor layer
+dxf.layers.append(sdxf.Layer(name="STATOR", color=5) )  #blue stator layer
+
+# Add text to describe the design
+# later dxf.append( sdxf.Text( "lobes="       +str(l), point=(p*n+d, 0.7), layer="text", height=0.1) )
+
+# angle steps = lines in dxf profiles
+s = 100
+q=2*math.pi/float(s)
+
+#generate the rotor profile
+#xhypo(th) := 3*cos(th) + cos(3*th) $
+def xhypo(t):
+    return 5*math.cos(t)+math.cos(5*t)
+	
+#yhypo(th) := 3*sin(th) - sin(3*th) $
+def yhypo(t):
+    return 5*math.sin(t)- math.sin(5*t)
+	
+#xepi(th) := 5*cos(th) - cos(5*th) $
+def xepi(t):
+    return 7*math.cos(t)-math.cos(7*t)
+
+#yepi(th) := 5*sin(th) - sin(5*th) $
+def yepi(t):
+    return 7*math.sin(t)- math.sin(7*t)
+	
+def x(t):
+	if t >= 0 and t < math.pi/3:
+		return xhypo(t)
+	elif t >= math.pi/3 and t < 2*math.pi/3:
+		return xepi(t)
+	elif t >= 2*math.pi/3 and t < math.pi:
+		return xhypo(t)
+	elif t >= math.pi and t < 4*math.pi/3:
+		return xepi(t)
+	elif t >= 4*math.pi/3 and t < 5 * math.pi / 3:
+		return xhypo(t)
+	else:
+		return xepi(t)
+		
+def y(t):
+	if t >= 0 and t < math.pi/3:
+		return yhypo(t)
+	elif t >= math.pi/3 and t < 2*math.pi/3:
+		return yepi(t)
+	elif t >= 2*math.pi/3 and t < math.pi:
+		return yhypo(t)
+	elif t >= math.pi and t < 4*math.pi/3:
+		return yepi(t)
+	elif t >= 4*math.pi/3 and t < 5 * math.pi / 3:
+		return yhypo(t)
+	else:
+		return yepi(t)
+	
+#plot2d([[parametric,xctr(t),yctr(t),[t,0,2*%pi]],[parametric,xhypo(t),yhypo(t),[t,0,2*%pi]]],[nticks,80])
+
+i=0
+x1 = x(q*i)
+y1 = y(q*i)
+# s is the number of steps, q is 2 pi divided by s
+for i in range(0, s):
+        x2 = x(q*(i+1))
+        y2 = y(q*(i+1))
+        dxf.append( sdxf.Line(points=[(x1,y1),  (x2,y2)], layer="STATOR" ) )
+        x1 = x2
+        y1 = y2
+
+# need a file name
+f = "moinTriple.dxf"
+
+try:
+        dxf.saveas(f)
+except:
+        print("File save error!")
+        sys.exit(2)
+
+
